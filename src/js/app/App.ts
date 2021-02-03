cb28b6e39e0d215fba0017b316f30d6881d85a45
szarlata <e.a.szarlata@gmail.com>
2021-02-03 21:29:16 +0100

    Button with microphone icon created as an component with seperate ts and scss files. Minor changes in index.html and styles.scss to display elements ahead of wrapper

--- src/js/app/App.ts
+++ src/js/app/App.ts
@@ -1,5 +1,7 @@
 import Search from './components/Search';
+import ButtonMic from './components/ButtonMic';
 
 export const App = ({ options }) => {
   new Search().render();
+  new ButtonMic().render();
 };
