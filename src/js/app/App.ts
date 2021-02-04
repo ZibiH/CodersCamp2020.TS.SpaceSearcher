81a45e762608a4dd105c7e33f364519ac9be6dc4
Dave The Web Dev <dantczak211@law.edu.pl>
2021-02-04 15:11:25 +0100

    changed names of files

--- src/js/app/App.ts
+++ src/js/app/App.ts
@@ -1,7 +1,7 @@
 import Search from './components/Search';
-import SpeechRecognition from './components/SpeechRecognition';
+import SpeechRecording from './components/SpeechRecording';
 
 export const App = ({ options }:any) => {
   const searchBar = new Search().render();
-  new SpeechRecognition().startRecording(searchBar) // to będzie trzeba wrzucic po nacisnięciu na mikrofon 
+  new SpeechRecording().startRecording(searchBar) // to będzie trzeba wrzucic po nacisnięciu na mikrofon 
 };
--- src/js/app/components/Search.ts
+++ src/js/app/components/Search.ts
@@ -4,12 +4,9 @@ class Search {
   constructor() {}
 
   render() {
-    const app: HTMLElement = document.querySelector('#app');
-
     const input = renderElement({
       tagHTML: 'input',
       className: 'search',
-      parentElement: app,
     });
 
     return input;
